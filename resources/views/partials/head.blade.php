b2afcc38421319e6c20205c97047275aafd6f17c
Patrick Caminhas <42319923+PatrickCaminhas@users.noreply.github.com>
2024-08-12 16:41:48 -0300

    Serviços
    Alteração na view de  cadastro de serviços:
    -Não há mais data final  (a fim de evitar erros de data fim ser menor que data inicio e o inverso)
    -Usuário pode optar por escolher o tipo de prestação de serviço.
    -Se e o usuário escolher por dias, agora tem a quantidade de dias de serviço após o inicio.
    -Agora usuário escolher por horas, vai ser informado a quantidade de minutos que o serviço demanda e ele escolhe a quantidade de horas e/ou minutos que ele vai exercer naquele serviço, assim é calculado valor de cada minuto do serviço e multiplicado pelo tempo escolhido.
    -Continua informando o valor do serviço ao final de escolher tipo de serviço, data de inicio e as informações da prestação do serviço.
    
    -Colocado Favicon da aplicação e aplicada na partial do header,

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,7 @@
 <title>{{$empresa}}
      - Simplifiq</title>
 <!-- Inclua os arquivos CSS do Bootstrap -->
+<link rel="shortcut icon" type="imagex/png" href="{{ global_asset('favicon.ico') }}">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- resources/views/sistema/servicos/servicosCadastro.blade.php
+++ resources/views/sistema/servicos/servicosCadastro.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - Simplifiq</title>
-    <!-- Inclua os arquivos CSS do Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,7 +12,6 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.2/css/responsive.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/plug-ins/2.0.8/i18n/pt-BR.json">
-
 </head>
 
 <body class="bg-black bg-gradient">
@@ -45,7 +43,7 @@
                                             </div>
                                             <div class="form-floating mb-3">
                                                 <input type="text" class="form-control" name="nome_cliente"
-                                                    id="nome" placeholder="" required>
+                                                    id="nome_cliente" placeholder="" required>
                                                 <label>Nome do cliente:</label>
                                             </div>
                                             <label for="tipo_cliente">Tipo de cliente:</label>
@@ -67,14 +65,14 @@
                                                 <div id="identificadorHelp" class="form-text">Use somente números.</div>
                                             </div>
 
-
                                             <div class="form-floating mb-3">
                                                 <select class="form-select" id="tipo_servico"
                                                     aria-label="Floating label select example">
                                                     <option selected disabled>Selecione o tipo de serviço</option>
                                                     @foreach ($tipos_servico as $tipo_servico)
                                                         <option value="{{ $tipo_servico->id }}"
-                                                            data-valor="{{ $tipo_servico->valor_diario }}">
+                                                            data-valor="{{ $tipo_servico->valor_diario }}"
+                                                            data-duracao="{{ $tipo_servico->duracao }}">
                                                             {{ $tipo_servico->nome }}</option>
                                                     @endforeach
                                                 </select>
@@ -86,36 +84,72 @@
                                                     id="data_inicio" min="{{ now()->format('Y-m-d') }}" required>
                                                 <label>Data de início:</label>
                                             </div>
-                                            <div class="form-floating mb-3">
-                                                <input type="date" class="form-control" name="data_fim"
-                                                    id="data_fim" min="{{ now()->format('Y-m-d') }}" required>
-                                                <label>Data do fim:</label>
+
+                                            <label for="tipo_prestacao_servico">Tipo de prestação de serviço:</label>
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio"
+                                                    name="tipo_prestacao_servico" id="tipo_prestacao_servico_dias"
+                                                    value="Dias">
+                                                <label class="form-check-label" for="Dias">Dias</label>
                                             </div>
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio"
+                                                    name="tipo_prestacao_servico" id="tipo_prestacao_servico_horas"
+                                                    value="Horas">
+                                                <label class="form-check-label" for="Horas">Horas</label>
+                                            </div>
+
+                                            <!-- Campos Dinâmicos -->
+                                            <div id="campo_dias_servico" class="form-floating mb-3 d-none">
+                                                <input type="number" class="form-control" name="dias_servico"
+                                                    id="dias_servico"
+                                                    placeholder="Informe a quantidade de dias para o serviço">
+                                                <label>Dias de serviço:</label>
+                                            </div>
+
+                                            <div id="campo_horas_servico" class="row d-none">
+                                                <label for="mediaTempo">Tempo para esse tipo de serviço: <span
+                                                    id="duracao_servico"></span> minutos</label>
+
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3">
+                                                        <input type="number" class="form-control" name="horas"
+                                                            id="horas" placeholder="Quantidade de horas">
+                                                        <label>Horas:</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3">
+                                                        <input type="number" class="form-control" name="minutos"
+                                                            id="minutos" placeholder="Quantidade de minutos">
+                                                        <label>Minutos:</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+
                                             <div class="form-group">
-                                                <p><label for="valor">Valor: <span id="valor_servico"></span></label>
+                                                <p><label for="valor">Valor: <span
+                                                            id="valor_servico"></span></label>
                                                 </p>
                                                 <input type="hidden" name="valor" id="valor">
-
                                             </div>
 
                                             <div class="form-floating">
                                                 <textarea class="form-control" placeholder="Descrição do serviço" id="descricao" name="descricao"></textarea>
-                                                <label for="Descricao">Descrição:</label>
+                                                <label for="descricao">Descrição:</label>
                                             </div>
-                                            <div class=" text-center mt-3">
+                                            <div class="text-center mt-3">
                                                 <button type="submit" class="btn btn-success">Cadastrar</button>
                                             </div>
                                         </form>
+
                                         @if ($errors->any())
                                             <div class="alert alert-danger mt-3">
-
                                                 @foreach ($errors->all() as $error)
                                                     {{ $error }}
                                                 @endforeach
-
                                             </div>
                                         @endif
-
 
                                     </div>
                                 </div>
@@ -126,63 +160,71 @@
             </div>
         </div>
     </div>
-    <!-- Features Section -->
 
-    <!-- Inclua os arquivos JavaScript do Bootstrap -->
-
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/3.0.2/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/3.0.2/js/responsive.bootstrap5.min.js"></script>
+
     <script>
         $(document).ready(function() {
-            $('#myTable').DataTable({
-                language: {
-                    url: 'https://cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json'
-                },
+            // Mostrar campos dinâmicos com base na seleção do tipo de prestação de serviço
+            $('input[name="tipo_prestacao_servico"]').change(function() {
+                if ($(this).val() === 'Dias') {
+                    $('#campo_dias_servico').removeClass('d-none');
+                    $('#campo_horas_servico').addClass('d-none');
+                } else if ($(this).val() === 'Horas') {
+                    $('#campo_horas_servico').removeClass('d-none');
+                    $('#campo_dias_servico').addClass('d-none');
+                }
+                calcularValor(); // Recalcular valor ao mudar o tipo de prestação de serviço
             });
-        });
-    </script>
-    <script>
-        function calcularValor() {
-            var tipoServicoSelect = document.getElementById('tipo_servico');
-            var dataInicio = document.querySelector('input[name="data_inicio"]').value;
-            var dataFim = document.querySelector('input[name="data_fim"]').value;
-            var selectedOption = tipoServicoSelect.options[tipoServicoSelect.selectedIndex];
-            var valorDiario = selectedOption.getAttribute('data-valor');
 
-            if (valorDiario && dataInicio && dataFim) {
-                var inicio = new Date(dataInicio);
-                var fim = new Date(dataFim);
-                var diffTime = Math.abs(fim - inicio);
-                var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
+            // Limitar valores de horas e minutos
+            $('#horas').attr({
+                'min': 0,
+                'max': 8
+            });
+            $('#minutos').attr({
+                'min': 0,
+                'max': 59
+            });
 
-                var valorTotal = valorDiario * diffDays;
+            // Recalcular valor ao mudar o tipo de serviço ou quantidade de dias/horas/minutos
+            $('#tipo_servico, #dias_servico, #horas, #minutos').change(calcularValor);
+            $('#data_inicio').change(calcularValor);
+
+            function calcularValor() {
+                var tipoServicoSelect = document.getElementById('tipo_servico');
+                var selectedOption = tipoServicoSelect.options[tipoServicoSelect.selectedIndex];
+                var valorDiario = parseFloat(selectedOption.getAttribute('data-valor'));
+                var duracaoDiario = parseFloat(selectedOption.getAttribute('data-duracao'));
+
+                var tipoPrestacao = $('input[name="tipo_prestacao_servico"]:checked').val();
+                var valorTotal = 0;
+
+
+                if (tipoPrestacao === 'Dias') {
+                    var dias = parseInt(document.getElementById('dias_servico').value);
+                    if (dias > 0) {
+                        valorTotal = valorDiario * dias;
+                    }
+                } else if (tipoPrestacao === 'Horas') {
+
+                    var horas = parseInt(document.getElementById('horas').value);
+                    var minutos = parseInt(document.getElementById('minutos').value);
+                    if (horas >= 0 && minutos >= 0) {
+                        var valorPorMinuto = valorDiario / duracaoDiario;
+                        var tempoTotalMinutos = (horas * 60) + minutos;
+                        valorTotal = valorPorMinuto * tempoTotalMinutos;
+                    }
+                }
+
                 document.getElementById('valor_servico').textContent = 'R$ ' + valorTotal.toFixed(2);
                 document.getElementById('valor').value = valorTotal.toFixed(2);
             }
-        }
-
-        function ajustarDatas() {
-            var dataInicio = document.getElementById('data_inicio').value;
-            var dataFim = document.getElementById('data_fim').value;
-
-            if (dataInicio) {
-                document.getElementById('data_fim').min = dataInicio;
-            }
-
-            if (dataFim) {
-                document.getElementById('data_inicio').max = dataFim;
-            }
-
-            calcularValor();
-        }
-
-        document.getElementById('tipo_servico').addEventListener('change', calcularValor);
-        document.getElementById('data_inicio').addEventListener('change', ajustarDatas);
-        document.getElementById('data_fim').addEventListener('change', ajustarDatas);
+        });
     </script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
@@ -207,7 +249,6 @@
             });
         });
     </script>
-
 
 </body>
 
